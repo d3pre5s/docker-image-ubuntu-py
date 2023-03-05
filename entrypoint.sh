578bb67330b24ead0c4fded4e9d39cc13027fedb
Дипеж <d3pre5s@ya.ru>
2023-03-05 13:04:50 +0300

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
-#!/bin/ash
+#!/bin/bash
 
 # setup ssh-key for runner access to host
 echo ${SSH_KEY} | base64 -d > /sshkey
 chmod 600 /sshkey
-
-# add gitlab-ci-token to requirements.yml
-find /builds -name requirements.yml | xargs sed 's#https://#https://gitlab-ci-token:${CI_JOB_TOKEN}@#g' -i 
 
 exec "$@"
